c67113ef2d425de32d688e26a827f699b09e6773
John Doe <annghdev@gmail.com>
2026-03-17 23:35:07 +0700

    Add Nuget packages and reference projects

--- src/Aspire.AppHost/AppHost.cs
+++ src/Aspire.AppHost/AppHost.cs
@@ -2,4 +2,10 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Projects.APIGateway>("apigateway");
 
+builder.AddProject<Projects.Catalog_API>("catalog-api");
+
+builder.AddProject<Projects.Inventory_API>("inventory-api");
+
+builder.AddProject<Projects.Order_API>("order-api");
+
 builder.Build().Run();
